630049a65956e745bec409a1f76aea15ba7fe5fe
HongyuZh <435984671@qq.com>
2023-07-01 17:18:53 +0800

    feat: save params

--- profiling/data_fit.py
+++ profiling/data_fit.py
@@ -27,6 +27,9 @@ if __name__ == "__main__":
         params_inverse, _ = curve_fit(inverse_func, x, y)
         a_inverse, b_inverse = params_inverse
 
+        with open(f"data/{image}_fitting.pkl", "wb") as f:
+            pickle.dump(params_inverse, f)
+
         plt.figure()
         plt.scatter(x, y, label="Data")
         plt.plot(x, inverse_func(x, a_inverse, b_inverse), label="Inverse Fit")
